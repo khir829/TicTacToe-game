26eb9cccdccab7b83e39a197d403c405bf38afd9
Kai <khir829@aucklanduni.ac.nz>
2022-07-19 23:37:14 +1200

    Add comments to code

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,11 +1,12 @@
 package game;
+
 import java.util.Scanner;
 
 public class Main {
 	public static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-
+		// initialise and start Tic Tac Toe game
 		TicTacToe game = new TicTacToe();
 		game.startGame();
 	}
