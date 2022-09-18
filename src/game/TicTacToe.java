446bc88a56e9bff8927d625ac421403080ea9708
Kai <khir829@aucklanduni.ac.nz>
2022-09-18 18:32:42 +1200

    refactor: relocate fields in Tic Tac Toe class

--- src/game/TicTacToe.java
+++ src/game/TicTacToe.java
@@ -3,232 +3,235 @@ package game;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 import strategyAI.AI;
 import strategyAI.StrategyAI;
 import strategyAI.StrategyFactory;
 
 public class TicTacToe {
-	private char[][] gameBoard = { { ' ', '|', ' ', '|', ' ' }, { '-', '+', '-', '+', '-' },
-			{ ' ', '|', ' ', '|', ' ' }, { '-', '+', '-', '+', '-' }, { ' ', '|', ' ', '|', ' ' } };
-	private List<List<Integer>> winCondition = Arrays.asList(Arrays.asList(1, 2, 3), Arrays.asList(4, 5, 6),
-			Arrays.asList(7, 8, 9), Arrays.asList(1, 4, 7), Arrays.asList(2, 5, 8), Arrays.asList(3, 6, 9),
-			Arrays.asList(1, 5, 9), Arrays.asList(7, 5, 3));
+  private char[][] gameBoard = {{' ', '|', ' ', '|', ' '}, {'-', '+', '-', '+', '-'},
+      {' ', '|', ' ', '|', ' '}, {'-', '+', '-', '+', '-'}, {' ', '|', ' ', '|', ' '}};
+  private List<List<Integer>> winCondition =
+      Arrays.asList(Arrays.asList(1, 2, 3), Arrays.asList(4, 5, 6), Arrays.asList(7, 8, 9),
+          Arrays.asList(1, 4, 7), Arrays.asList(2, 5, 8), Arrays.asList(3, 6, 9),
+          Arrays.asList(1, 5, 9), Arrays.asList(7, 5, 3));
 
-	private ArrayList<Integer> player1Pos = new ArrayList<Integer>();
-	private ArrayList<Integer> player2Pos = new ArrayList<Integer>();
-	private boolean gameEnd = false;
+  private ArrayList<Integer> player1Pos = new ArrayList<Integer>();
+  private ArrayList<Integer> player2Pos = new ArrayList<Integer>();
+  AI ai = new AI();
+  private String strategy;
+  private StrategyAI strategyOfAI;
+  private boolean gameEnd = false;
 
-	/**
-	 * This method prints out the game board state in a human readable way
-	 * 
-	 * @param gameBoard the state of the gamme
-	 */
-	public static void printGameBoard(char[][] gameBoard) {
-		for (char[] row : gameBoard) {
-			for (char c : row) {
-				System.out.print(c);
-			}
-			System.out.println();
-		}
-	}
+  /**
+   * This method prints out the game board state in a human readable way
+   * 
+   * @param gameBoard the state of the gamme
+   */
+  public static void printGameBoard(char[][] gameBoard) {
+    for (char[] row : gameBoard) {
+      for (char c : row) {
+        System.out.print(c);
+      }
+      System.out.println();
+    }
+  }
 
-	/**
-	 * This method starts the Tic Tac Toe game
-	 */
-	public void startGame() {
+  /**
+   * This method starts the Tic Tac Toe game
+   */
+  public void startGame() {
 
-		String gameMode;
+    String gameMode;
 
-		// Welcome and initilisation message
-		System.out.println("Welcome to the Tic Tac Toe game application!");
-		System.out.println("Top row 1-3 from left to right\n" + "Middle row 4-6 from left to right\n"
-				+ "Bottom row 7-9 from left to right");
+    // Welcome and initilisation message
+    System.out.println("Welcome to the Tic Tac Toe game application!");
+    System.out.println("Top row 1-3 from left to right\n" + "Middle row 4-6 from left to right\n"
+        + "Bottom row 7-9 from left to right");
 
-		gameMode = getGameMode();
+    gameMode = getGameMode();
 
-		// A 2 player game or an AI game is determined by user input.
-		// If it is a AI game, the AI is initialised
-		if (gameMode.equals("p")) {
-			printGameBoard(gameBoard);
-			selectedGameMode(null);
-		} else {
-			AI ai = new AI();
-			String string = getAIStrategy();
-			StrategyAI strategy = StrategyFactory.setUp(string);
-			ai.setStrategy(strategy);
-			printGameBoard(gameBoard);
-			selectedGameMode(ai);
-		}
+    // A 2 player game or an AI game is determined by user input.
+    // If it is a AI game, the AI is initialised
+    if (gameMode.equals("p")) {
+      printGameBoard(gameBoard);
+      selectedGameMode(null);
+    } else {
+      strategy = getAIStrategy();
+      strategyOfAI = StrategyFactory.setUp(strategy);
+      ai.setStrategy(strategyOfAI);
+      printGameBoard(gameBoard);
+      selectedGameMode(ai);
+    }
 
-	}
+  }
 
-	/**
-	 * This method obtains the AI strategy based on user input
-	 * 
-	 * @return the selected AI strategy
-	 */
-	private String getAIStrategy() {
-		System.out.println("Choose AI difficulty: Normal (N) - Unbeatable (U)");
-		String result = Main.scanner.next().toUpperCase();
-		while (!result.equals("N") && !result.equals("U")) {
-			System.out.println("Please type either 'N' or 'U' for Normal difficulty or Unbeatable difficulty");
-			result = Main.scanner.next();
-		}
-		return result;
-	}
+  /**
+   * This method obtains the AI strategy based on user input
+   * 
+   * @return the selected AI strategy
+   */
+  private String getAIStrategy() {
+    System.out.println("Choose AI difficulty: Normal (N) - Unbeatable (U)");
+    String result = Main.scanner.next().toUpperCase();
+    while (!result.equals("N") && !result.equals("U")) {
+      System.out
+          .println("Please type either 'N' or 'U' for Normal difficulty or Unbeatable difficulty");
+      result = Main.scanner.next();
+    }
+    return result;
+  }
 
-	/**
-	 * This method determines whether the Tic Tac Toe game is played with an AI or
-	 * with 2 players
-	 * 
-	 * @return the selected game mode
-	 */
-	public String getGameMode() {
-		String result;
-		System.out.println("Choose game mode: Two player game (P) - Play against AI (A)");
-		result = Main.scanner.next();
-		while (!result.equals("p") && !result.equals("a")) {
-			System.out.println("Please type either 'P' or 'A' to have a two player game or match against an AI");
-			result = Main.scanner.next();
-		}
-		return result.toLowerCase();
-	}
+  /**
+   * This method determines whether the Tic Tac Toe game is played with an AI or with 2 players
+   * 
+   * @return the selected game mode
+   */
+  public String getGameMode() {
+    String result;
+    System.out.println("Choose game mode: Two player game (P) - Play against AI (A)");
+    result = Main.scanner.next();
+    while (!result.equals("p") && !result.equals("a")) {
+      System.out.println(
+          "Please type either 'P' or 'A' to have a two player game or match against an AI");
+      result = Main.scanner.next();
+    }
+    return result.toLowerCase();
+  }
 
-	/**
-	 * This method initialises the game when based on game mode
-	 * 
-	 * (2 player or against AI)
-	 * 
-	 * @param ai the AI with a given strategy
-	 */
-	public void selectedGameMode(AI ai) {
-		boolean flag = true;
-		String otherPlayer = "Player 2";
+  /**
+   * This method initialises the game when based on game mode
+   * 
+   * (2 player or against AI)
+   * 
+   * @param ai the AI with a given strategy
+   */
+  public void selectedGameMode(AI ai) {
+    boolean flag = true;
+    String otherPlayer = "Player 2";
 
-		if (ai != null) {
-			otherPlayer = "AI";
-		}
+    if (ai != null) {
+      otherPlayer = "AI";
+    }
 
-		do {
-			getPlacement("Player 1", null);
-			if (gameEnd) {
-				flag = false;
-				System.out.println("Player 1 " + player1Pos);
-				System.out.println(otherPlayer + " " + player2Pos);
-				System.out.println(checkWinner(otherPlayer));
-				break;
-			}
+    do {
+      getPlacement("Player 1", null);
+      if (gameEnd) {
+        flag = false;
+        System.out.println("Player 1 " + player1Pos);
+        System.out.println(otherPlayer + " " + player2Pos);
+        System.out.println(checkWinner(otherPlayer));
+        break;
+      }
 
-			getPlacement(otherPlayer, ai);
-			if (gameEnd) {
-				flag = false;
-				System.out.println("Player 1 " + player1Pos);
-				System.out.println(otherPlayer + " " + player2Pos);
-				System.out.println(checkWinner(otherPlayer));
-				break;
-			}
-		} while (flag);
-	}
+      getPlacement(otherPlayer, ai);
+      if (gameEnd) {
+        flag = false;
+        System.out.println("Player 1 " + player1Pos);
+        System.out.println(otherPlayer + " " + player2Pos);
+        System.out.println(checkWinner(otherPlayer));
+        break;
+      }
+    } while (flag);
+  }
 
-	/**
-	 * This method obtains the placement of a symbol by the player
-	 * 
-	 * @param player the current player
-	 */
-	public void getPlacement(String player, AI ai) {
-		int pos;
-		if (ai == null) {
-			System.out.println("\n" + player + " Enter your placement (1-9): \n");
-			pos = Main.scanner.nextInt();
+  /**
+   * This method obtains the placement of a symbol by the player
+   * 
+   * @param player the current player
+   */
+  public void getPlacement(String player, AI ai) {
+    int pos;
+    if (ai == null) {
+      System.out.println("\n" + player + " Enter your placement (1-9): \n");
+      pos = Main.scanner.nextInt();
 
-			while (player1Pos.contains(pos) || player2Pos.contains(pos) || pos < 1 || pos > 9) {
-				printGameBoard(gameBoard);
-				System.out.println("Enter a valid position");
-				pos = Main.scanner.nextInt();
-			}
-			placePiece(gameBoard, pos, player);
-			checkWinner("player");
-			printGameBoard(gameBoard);
-		} else {
-			placePiece(gameBoard, ai.placement(player1Pos, player2Pos), "AI");
-			checkWinner("AI");
-			System.out.println("\nAI makes a move\n");
-			printGameBoard(gameBoard);
-		}
-	}
+      while (player1Pos.contains(pos) || player2Pos.contains(pos) || pos < 1 || pos > 9) {
+        printGameBoard(gameBoard);
+        System.out.println("Enter a valid position");
+        pos = Main.scanner.nextInt();
+      }
+      placePiece(gameBoard, pos, player);
+      checkWinner("player");
+      printGameBoard(gameBoard);
+    } else {
+      placePiece(gameBoard, ai.placement(player1Pos, player2Pos), "AI");
+      checkWinner("AI");
+      System.out.println("\nAI makes a move\n");
+      printGameBoard(gameBoard);
+    }
+  }
 
-	/**
-	 * This method check who the winner is. If there is no winner, it is a draw
-	 * 
-	 * @return The message stating who won or if it was a draw
-	 */
-	public String checkWinner(String otherPlayer) {
+  /**
+   * This method check who the winner is. If there is no winner, it is a draw
+   * 
+   * @return The message stating who won or if it was a draw
+   */
+  public String checkWinner(String otherPlayer) {
 
-		for (List<Integer> list : winCondition) {
-			if (player1Pos.containsAll(list)) {
-				gameEnd = true;
-				return "Player 1 Wins!";
-			} else if (player2Pos.containsAll(list)) {
-				gameEnd = true;
-				return otherPlayer + " Wins!";
-			}
-		}
-		if (player1Pos.size() + player2Pos.size() == 9) {
-			gameEnd = true;
-			return "It's a draw!";
-		}
-		return "";
-	}
+    for (List<Integer> list : winCondition) {
+      if (player1Pos.containsAll(list)) {
+        gameEnd = true;
+        return "Player 1 Wins!";
+      } else if (player2Pos.containsAll(list)) {
+        gameEnd = true;
+        return otherPlayer + " Wins!";
+      }
+    }
+    if (player1Pos.size() + player2Pos.size() == 9) {
+      gameEnd = true;
+      return "It's a draw!";
+    }
+    return "";
+  }
 
-	/**
-	 * This method places the symbol on the Tic Tac Toe board
-	 * 
-	 * @param gameBoard the current state of the game board
-	 * @param pos       the position of the placement
-	 * @param user      the player
-	 */
-	public void placePiece(char[][] gameBoard, int pos, String user) {
+  /**
+   * This method places the symbol on the Tic Tac Toe board
+   * 
+   * @param gameBoard the current state of the game board
+   * @param pos the position of the placement
+   * @param user the player
+   */
+  public void placePiece(char[][] gameBoard, int pos, String user) {
 
-		char symbol = 'X';
+    char symbol = 'X';
 
-		if (user.equals("Player 1")) {
-			player1Pos.add(pos);
-			symbol = 'X';
-		} else {
-			player2Pos.add(pos);
-			symbol = 'O';
-		}
+    if (user.equals("Player 1")) {
+      player1Pos.add(pos);
+      symbol = 'X';
+    } else {
+      player2Pos.add(pos);
+      symbol = 'O';
+    }
 
-		switch (pos) {
-		case 1:
-			gameBoard[0][0] = symbol;
-			break;
-		case 2:
-			gameBoard[0][2] = symbol;
-			break;
-		case 3:
-			gameBoard[0][4] = symbol;
-			break;
-		case 4:
-			gameBoard[2][0] = symbol;
-			break;
-		case 5:
-			gameBoard[2][2] = symbol;
-			break;
-		case 6:
-			gameBoard[2][4] = symbol;
-			break;
-		case 7:
-			gameBoard[4][0] = symbol;
-			break;
-		case 8:
-			gameBoard[4][2] = symbol;
-			break;
-		case 9:
-			gameBoard[4][4] = symbol;
-			break;
-		}
-	}
+    switch (pos) {
+      case 1:
+        gameBoard[0][0] = symbol;
+        break;
+      case 2:
+        gameBoard[0][2] = symbol;
+        break;
+      case 3:
+        gameBoard[0][4] = symbol;
+        break;
+      case 4:
+        gameBoard[2][0] = symbol;
+        break;
+      case 5:
+        gameBoard[2][2] = symbol;
+        break;
+      case 6:
+        gameBoard[2][4] = symbol;
+        break;
+      case 7:
+        gameBoard[4][0] = symbol;
+        break;
+      case 8:
+        gameBoard[4][2] = symbol;
+        break;
+      case 9:
+        gameBoard[4][4] = symbol;
+        break;
+    }
+  }
 
 }
--- src/strategyAI/UnbeatableStrategy.java
+++ src/strategyAI/UnbeatableStrategy.java
@@ -1,4 +1,5 @@
 package strategyAI;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -6,113 +7,114 @@ import java.util.List;
 import java.util.Map;
 
 public class UnbeatableStrategy implements StrategyAI {
-	private List<List<Integer>> winCondition = Arrays.asList(Arrays.asList(1, 2, 3), Arrays.asList(4, 5, 6),
-			Arrays.asList(7, 8, 9), Arrays.asList(1, 4, 7), Arrays.asList(2, 5, 8), Arrays.asList(3, 6, 9),
-			Arrays.asList(1, 5, 9), Arrays.asList(7, 5, 3));
+  private List<List<Integer>> winCondition =
+      Arrays.asList(Arrays.asList(1, 2, 3), Arrays.asList(4, 5, 6), Arrays.asList(7, 8, 9),
+          Arrays.asList(1, 4, 7), Arrays.asList(2, 5, 8), Arrays.asList(3, 6, 9),
+          Arrays.asList(1, 5, 9), Arrays.asList(7, 5, 3));
 
-	@Override
-	public int SymbolPlacement(ArrayList<Integer> player1Pos, ArrayList<Integer> AIPos) {
-		int bestMove = minimax(player1Pos, AIPos)[1];
-		return bestMove;
-	}
+  @Override
+  public int SymbolPlacement(ArrayList<Integer> player1Pos, ArrayList<Integer> AIPos) {
+    int bestMove = minimax(player1Pos, AIPos)[1];
+    return bestMove;
+  }
 
-	/**
-	 * This method returns a score based on the positions of the AI and the player
-	 * 
-	 * @param player1Pos The current positions of symbols of the first player
-	 * @param AIPosThe   current positions of symbols of the AI
-	 * @return the score of the placement
-	 */
-	public int[] minimax(ArrayList<Integer> player1Pos, ArrayList<Integer> AIPos) {
-		ArrayList<Integer> newPlayerMoves = player1Pos;
-		ArrayList<Integer> newAIMoves = AIPos;
-		ArrayList<Integer> emptySpots = new ArrayList<Integer>();
-		Map<Integer, Integer> moveList = new HashMap<Integer, Integer>();
+  /**
+   * This method returns a score based on the positions of the AI and the player
+   * 
+   * @param player1Pos The current positions of symbols of the first player
+   * @param AIPosThe current positions of symbols of the AI
+   * @return the score of the placement
+   */
+  public int[] minimax(ArrayList<Integer> player1Pos, ArrayList<Integer> AIPos) {
+    ArrayList<Integer> newPlayerMoves = player1Pos;
+    ArrayList<Integer> newAIMoves = AIPos;
+    ArrayList<Integer> emptySpots = new ArrayList<Integer>();
+    Map<Integer, Integer> moveList = new HashMap<Integer, Integer>();
 
-		int[] result = new int[2];
-		int[] best = new int[2];
-		int bestScore;
+    int[] result = new int[2];
+    int[] best = new int[2];
+    int bestScore;
 
-		// Create list of positions that are available on the gameboard
-		for (int i = 1; i <= 9; i++) {
-			if (!player1Pos.contains(i) && !AIPos.contains(i)) {
-				emptySpots.add(i);
-			}
-		}
+    // Create list of positions that are available on the gameboard
+    for (int i = 1; i <= 9; i++) {
+      if (!player1Pos.contains(i) && !AIPos.contains(i)) {
+        emptySpots.add(i);
+      }
+    }
 
-		// Check if there is a winner or a tie and return appropriate values for the
-		// minimax algorithm
-		if (checkWinner(player1Pos)) {
-			result[0] = (-1 * (emptySpots.size() + 1));
-			return result;
-		} else if (checkWinner(AIPos)) {
-			result[0] = (1 * (emptySpots.size() + 1));
-			return result;
-		} else if (emptySpots.size() == 0) {
-			result[0] = 0;
-			return result;
-		}
+    // Check if there is a winner or a tie and return appropriate values for the
+    // minimax algorithm
+    if (checkWinner(player1Pos)) {
+      result[0] = (-1 * (emptySpots.size() + 1));
+      return result;
+    } else if (checkWinner(AIPos)) {
+      result[0] = (1 * (emptySpots.size() + 1));
+      return result;
+    } else if (emptySpots.size() == 0) {
+      result[0] = 0;
+      return result;
+    }
 
-		// Loop through empty positions on board and add AI or player positions on the
-		// board recursively
-		for (int i = 0; i < emptySpots.size(); i++) {
-			int score;
+    // Loop through empty positions on board and add AI or player positions on the
+    // board recursively
+    for (int i = 0; i < emptySpots.size(); i++) {
+      int score;
 
-			if (player1Pos.size() == AIPos.size()) {
+      if (player1Pos.size() == AIPos.size()) {
 
-				newPlayerMoves.add(emptySpots.get(i));
-				score = minimax(newPlayerMoves, AIPos)[0];
-				moveList.put(emptySpots.get(i), score);
-				newPlayerMoves.remove(emptySpots.get(i));
+        newPlayerMoves.add(emptySpots.get(i));
+        score = minimax(newPlayerMoves, AIPos)[0];
+        moveList.put(emptySpots.get(i), score);
+        newPlayerMoves.remove(emptySpots.get(i));
 
-			} else {
+      } else {
 
-				newAIMoves.add(emptySpots.get(i));
-				score = minimax(player1Pos, newAIMoves)[0];
-				moveList.put(emptySpots.get(i), score);
-				newAIMoves.remove(emptySpots.get(i));
+        newAIMoves.add(emptySpots.get(i));
+        score = minimax(player1Pos, newAIMoves)[0];
+        moveList.put(emptySpots.get(i), score);
+        newAIMoves.remove(emptySpots.get(i));
 
-			}
-		}
+      }
+    }
 
-		// Get the best score
-		if (player1Pos.size() == AIPos.size()) {
-			bestScore = 1000;
-			for (int key : moveList.keySet()) {
-				if (moveList.get(key) < bestScore) {
-					bestScore = moveList.get(key);
-					best[0] = moveList.get(key);
-					best[1] = key;
-				}
-			}
-		} else {
-			bestScore = -1000;
-			for (int key : moveList.keySet()) {
-				if (moveList.get(key) > bestScore) {
-					bestScore = moveList.get(key);
-					best[0] = moveList.get(key);
-					best[1] = key;
-				}
-			}
-		}
-		return best;
-	}
+    // Get the best score
+    if (player1Pos.size() == AIPos.size()) {
+      bestScore = 1000;
+      for (int key : moveList.keySet()) {
+        if (moveList.get(key) < bestScore) {
+          bestScore = moveList.get(key);
+          best[0] = moveList.get(key);
+          best[1] = key;
+        }
+      }
+    } else {
+      bestScore = -1000;
+      for (int key : moveList.keySet()) {
+        if (moveList.get(key) > bestScore) {
+          bestScore = moveList.get(key);
+          best[0] = moveList.get(key);
+          best[1] = key;
+        }
+      }
+    }
+    return best;
+  }
 
-	/**
-	 * This method checks if there is a winner based on input positions of the
-	 * symbols and the winning conditions
-	 * 
-	 * @param pos the position of the symbols
-	 * @return true or false depending on the symbol positions
-	 */
-	public boolean checkWinner(ArrayList<Integer> pos) {
-		// Loop through the win condition and compare the positions
-		for (List<Integer> list : winCondition) {
-			if (pos.containsAll(list)) {
-				return true;
-			}
-		}
-		return false;
-	}
+  /**
+   * This method checks if there is a winner based on input positions of the symbols and the winning
+   * conditions
+   * 
+   * @param pos the position of the symbols
+   * @return true or false depending on the symbol positions
+   */
+  public boolean checkWinner(ArrayList<Integer> pos) {
+    // Loop through the win condition and compare the positions
+    for (List<Integer> list : winCondition) {
+      if (pos.containsAll(list)) {
+        return true;
+      }
+    }
+    return false;
+  }
 
 }
